3298ef2aebd715c523f4185726cfc4c091c253e4
Renan <renanplays@outlook.com.br>
2024-08-07 12:07:24 -0300

    alguns ajustes

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,7 @@
-console.log(process.env.HELLO)
+import http from 'http'
+
+const server = http.createServer((_req, res) => {
+    res.end('Hello!')
+})
+
+server.listen(3000, () => console.log(`Server running at http://${process.env.HOST}:${process.env.PORT}`))
